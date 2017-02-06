28f511d0bfc0b6fa1b3be31c200c8dd0cdf59d94
klod9288 <klod9288@gmail.com>
2017-02-06 21:26:42 +0700

    Templess Myproject

---
+++ app/src/main/java/songklodb/apptemples/MainActivity.java
@@ -0,0 +1,48 @@
+package songklodb.apptemples;
+
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ListView;
+
+public class MainActivity extends AppCompatActivity {
+    //Explicit
+
+    private ListView templesListView;
+    private Button AboutButton;
+    private static final String ulAbout = "https://www.facebook.com/Eekkasitnongkon/?ref=page_internal";
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        templesListView = (ListView) findViewById(R.id.listView);
+        AboutButton = (Button) findViewById(R.id.button2);
+        //conterlor of button
+        AboutButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //sound fefect
+                MediaPlayer mediaPlayer = MediaPlayer.create(getBaseContext(),
+                        R.raw.beep1);
+                mediaPlayer.start();
+                //Inten wepView
+                Intent intent = new Intent(Intent.ACTION_VIEW);
+                intent.setData(Uri.parse(ulAbout));
+                startActivities(intent);
+
+            }//on Click
+
+            private void startActivities(Intent intent) {
+
+            }
+        });
+
+    }
+}
---
+++ app/src/main/java/songklodb/apptemples/R.java
@@ -0,0 +1,7 @@
+package songklodb.apptemples;
+
+/**
+ * Created by Admin on 06-Feb-17.
+ */
+public class R {
+}
